d8521c007d57c2f82a6fa6acc869269ef35c261f
Max-kolt <max_kolts@inbox.ru>
2025-03-07 16:47:11 +0300

    fix bug with buttons

--- js/dress_toggle.js
+++ js/dress_toggle.js
@@ -1,15 +1,15 @@
-var button_dress = document.querySelector('.dress_toggle');
+var button_dress = document.querySelector(".dress_toggle");
 
 var girlsPhoto = document.querySelectorAll(".womphoto");
-var boysPhoto = document.querySelectorAll('.manphoto');
+var boysPhoto = document.querySelectorAll(".manphoto");
 
 button_dress.addEventListener("click", function tooglePhotos(e) {
-  console.log("togg");
   girlsPhoto.forEach(toggleElementClosed);
   boysPhoto.forEach(toggleElementClosed);
-  button.textContent = button.textContent == "мужчины" ? "женщины" : "мужчины";
+  button_dress.textContent =
+    button.textContent == "мужчины" ? "женщины" : "мужчины";
 });
 
-function toggleElementClosed (element) {
-    element.classList.toggle('closed')
+function toggleElementClosed(element) {
+  element.classList.toggle("closed");
 }
