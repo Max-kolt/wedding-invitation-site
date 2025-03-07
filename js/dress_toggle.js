195afcac082655400378207b5df4ea2c99b5e54c
Max-kolt <max_kolts@inbox.ru>
2025-03-07 16:38:29 +0300

    fix bug with buttons

--- js/dress_toggle.js
+++ js/dress_toggle.js
@@ -1,16 +1,14 @@
-var button = document.querySelector('.dress_toggle');
+var button_dress = document.querySelector('.dress_toggle');
 
 var girlsPhoto = document.querySelectorAll(".womphoto");
 var boysPhoto = document.querySelectorAll('.manphoto');
 
-button.addEventListener('click',
-    function tooglePhotos(e) {
-        console.log('togg');
-        girlsPhoto.forEach(toggleElementClosed);
-        boysPhoto.forEach(toggleElementClosed)
-        button.textContent = button.textContent == 'мужчины' ? 'женщины' : 'мужчины';
-    }
-)
+button_dress.addEventListener("click", function tooglePhotos(e) {
+  console.log("togg");
+  girlsPhoto.forEach(toggleElementClosed);
+  boysPhoto.forEach(toggleElementClosed);
+  button.textContent = button.textContent == "мужчины" ? "женщины" : "мужчины";
+});
 
 function toggleElementClosed (element) {
     element.classList.toggle('closed')
--- js/send_request.js
+++ js/send_request.js
@@ -1,23 +1,19 @@
-var button = document.querySelector('.send_request');
+var reuest_button = document.querySelector(".send_request");
 
+reuest_button.addEventListener("click", function (event) {
+  event.preventDefault();
+  var data = JSON.stringify({
+    name: document.getElementById("name").value,
+    present: document.getElementById("yes_present").checked,
+    marry: document.getElementById("yes_marry").checked,
+    twoday: document.getElementById("yes_want").checked,
+    food: document.getElementById("eats").value,
+  });
 
-button.addEventListener('click', 
-    function (event){
-        event.preventDefault();
-        var data = JSON.stringify({
-            name: document.getElementById("name").value,
-            present: document.getElementById('yes_present').checked,
-            marry: document.getElementById('yes_marry').checked,
-            twoday: document.getElementById('yes_want').checked,
-            food: document.getElementById("eats").value,
-        });
-        
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/server, true");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(data);
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/server, true");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(data);
 
-        console.log(data);
-    }
-)
-
+  console.log(data);
+});
